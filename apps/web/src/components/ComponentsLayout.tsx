844448da7fa063fdc679b36911af71f96a2dca3b
Ruslan Shayakhmetov <ruslan.shayakhmetov@edetek.com>
2025-10-02 22:55:58 +0700

    Added: more functionality to Sidebar

--- apps/web/src/components/ComponentsLayout.tsx
+++ apps/web/src/components/ComponentsLayout.tsx
@@ -1,11 +1,12 @@
 import { AppSidebar } from './AppSidebar';
-import { SidebarProvider } from '@pixkit/react';
+import { SidebarProvider, SidebarTrigger } from '@pixkit/react';
 import { Outlet } from 'react-router-dom';
 
 export const ComponentsLayout = () => (
     <SidebarProvider>
       <AppSidebar />
       <main>
+        <SidebarTrigger>Open Sidebar</SidebarTrigger>
         <Outlet />
       </main>
     </SidebarProvider>
